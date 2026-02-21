f5ea2cfba196e9183a484acff07dc4d50772665f
ayush0745 <ayushrathour800@gmail.com>
2026-02-21 15:35:41 +0530

    [Ayush Rathour] Refactor .Updates solution of problem

--- dsa-practice/leet-code-codebase/BalancedBinaryTree.java
+++ dsa-practice/leet-code-codebase/BalancedBinaryTree.java
@@ -1,19 +1,18 @@
 class Solution {
+
     public boolean isBalanced(TreeNode root) {
-        return helper(root) != -1;
+        if(root == null) return true;
+
+        int left = height(root.left);
+        int right = height(root.right);
+
+        if(Math.abs(left - right) > 1) return false;
+
+        return isBalanced(root.left) && isBalanced(root.right);
     }
 
-    public static int helper(TreeNode root) {
-        if (root == null) return 0;
-
-        int left = helper(root.left);
-        if (left == -1) return -1;
-
-        int right = helper(root.right);
-        if (right == -1) return -1;
-
-        if (Math.abs(left - right) > 1) return -1;
-
-        return Math.max(left, right) + 1;
+    public int height(TreeNode root){
+        if(root == null) return 0;
+        return Math.max(height(root.left), height(root.right)) + 1;
     }
 }
